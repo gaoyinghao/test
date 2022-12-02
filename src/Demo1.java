b5cf93ea38857f64850d8684b137d178cbe82921
gaoyinghao <gaoyinghaosx@163.com>
2022-12-02 23:17:52 +0800

    fore commit

--- src/Demo1.java
+++ src/Demo1.java
@@ -1,10 +1,6 @@
 public class Demo1 {
 
-
     public static void main(String[] args) {
         System.out.println("hello git");
-
-        System.out.println("say");
-        System.out.println("gaoyinghao");
     }
 }
