3d74c9ae2c94fc0a3375737d71130c0f5671a7ef
gaoyinghao <gaoyinghaosx@163.com>
2022-12-02 23:43:06 +0800

    dev1 commit

--- src/Demo1.java
+++ src/Demo1.java
@@ -3,5 +3,6 @@ public class Demo1 {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello git2");
+        System.out.println("dev1");
     }
 }
